3fbaf1682dd69b70fc281fdad317f25a980c0f73
nlinnenbank <nlinnenbank@824000e8-63cf-11de-a6e1-e33d94ba4d19>
2009-04-25 23:54:58 +0000

    Added doxygen mainpage documentation, and removed collaboration diagrams from the output.
    git-svn-id: https://freenos.googlecode.com/svn/trunk@30 824000e8-63cf-11de-a6e1-e33d94ba4d19

---
+++ include/Doxygen.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2009 Niek Linnenbank
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __DOXYGEN_H
+#define __DOXYGEN_H
+
+/**
+ * @mainpage FreeNOS Documentation
+ * @section About
+ *
+ * This documentation is for FreeNOS (Free Niek's Operating System), revision $Revision$.
+ * FreeNOS is an experimental operating system for learning purposes, licensed under the GPLv3.
+ *
+ * @see http://www.freenos.org/
+ * @see http://gplv3.fsf.org/
+ *
+ * @section building Building
+ *
+ * $ make clean all iso
+ *
+ * @section booting Booting
+ *
+ * $ make fqemu
+ *
+ * @section debugging Debugging
+ *
+ * $ make qemu
+ *
+ */
+
+#endif /* __DOXYGEN_H */
