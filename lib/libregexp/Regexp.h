545915a464689bb81fc41d7dbd2793c2ae883111
cbijlsma <cbijlsma@824000e8-63cf-11de-a6e1-e33d94ba4d19>
2009-06-09 18:51:34 +0000

    Added yours truly to the AUTHORS file and created some space for libregexp
    git-svn-id: https://freenos.googlecode.com/svn/trunk@85 824000e8-63cf-11de-a6e1-e33d94ba4d19

---
+++ lib/libregexp/Regexp.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2009 Coen Bijlsma
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIBREGEXP_REGEXP_H
+#define __LIBREGEXP_REGEXP_H
+
+#include "String.h"
+#include "string.h"
+
+/**
+ * This class privides regexp functionality
+ */
+class Regexp
+{
+    public:
+    
+	/**
+	 * Default constructor.
+	 */
+	Regexp();
+	
+	Regexp(String& pattern);
+	
+	Regexp(const char* pattern);
+
+    private:
+
+	const char* _pattern;
+};
+
+#endif /* __LIBREGEXP_REGEXP_H */
