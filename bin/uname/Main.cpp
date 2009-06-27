1628294336db88b4222c869565a92c3c8f9bc7ba
nlinnenbank <nlinnenbank@824000e8-63cf-11de-a6e1-e33d94ba4d19>
2009-06-27 04:49:11 +0000

    Added the command-line options -h, -n, -r, -v and -m to /bin/uname.
    git-svn-id: https://freenos.googlecode.com/svn/trunk@154 824000e8-63cf-11de-a6e1-e33d94ba4d19

--- bin/uname/Main.cpp
+++ bin/uname/Main.cpp
@@ -32,20 +32,72 @@ int main(int argc, char **argv)
 		argv[0], strerror(errno));
 	return errno;
     }
-    /* System name. */
-    if (argc <= 1 || !strcmp(argv[1], "-s"))
+    /* If no argument given, show the system name. */
+    if (argc <= 1)
     {
-	printf("%s\r\n", info.sysname);
+	printf("%s", info.sysname);
     }
-    /* Everything. */
-    else if (argc == 2 && !strcmp(argv[1], "-a"))
+    /* Otherwise, interpret argument(s). */
+    else
     {
-        printf("%s %s %s %s %s\r\n",
-                info.sysname,
-                info.nodename,
-                info.release,
-                info.version,
-                info.machine);
+	for (int i = 0; i < argc - 1; i++)
+	{
+	    /* System name. */
+	    if (strcmp(argv[i + 1], "-s") == 0)
+		printf("%s ", info.sysname);
+	
+	    /* Node name. */
+	    else if (strcmp(argv[i + 1], "-n") == 0)
+		printf("%s ", info.nodename);
+	
+	    /* Release. */
+	    else if (strcmp(argv[i + 1], "-r") == 0)
+		printf("%s ", info.release);
+	
+	    /* Version. */
+	    else if (strcmp(argv[i + 1], "-v") == 0)
+		printf("%s ", info.version);
+	
+	    /* Machine name. */
+	    else if (strcmp(argv[i + 1], "-m") == 0)
+		printf("%s ", info.machine);
+
+	    /* Everything. */
+	    else if (strcmp(argv[i + 1], "-a") == 0)
+	        printf("%s %s %s %s %s ",
+            		info.sysname,
+        	        info.nodename,
+        		info.release,
+            		info.version,
+            		info.machine);
+
+	    /* Help message. */
+	    else if (strcmp(argv[i + 1], "-h") == 0)
+	    {
+		printf( "usage: %s [OPTION]...\r\n"
+			"Print system information.\r\n"
+			"\r\n"
+			"-h     Show this help message\r\n"
+			"-a     Print all information in the following order:\r\n"
+			"-s     Print the system name.\r\n"
+			"-n     Print the network hostname.\r\n"
+			"-r     Print the kernel release.\r\n"
+			"-v     Print the kernel version.\r\n"
+			"-m     Print the machine name.\r\n", argv[0]);
+		return EXIT_SUCCESS;
+	    }
+	    /* Unknown argument. */
+	    else
+	    {
+		printf("%s: invalid argument `%s'\r\n",
+			argv[0], argv[i + 1]);
+		return EXIT_FAILURE;
+	    }
+	}
     }
+    /* Terminate with a newline. */
+    printf("\r\n");
+    
+    /* Done. */
     return EXIT_SUCCESS;
 }
