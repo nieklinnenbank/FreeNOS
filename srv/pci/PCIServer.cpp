3a35171bab7065e784f3d747cfe2d01e48b4f01b
nieklinnenbank <nieklinnenbank@824000e8-63cf-11de-a6e1-e33d94ba4d19>
2009-08-13 20:26:25 +0000

    (Re)implemented the PCI server as a FileSystem. It is accessible via /dev/pci, containing (sub)directories for each bus and slot detected via the PCI configuration space. For each detected device, there are at least the vendor, device, revision, interrupt, and bar0-bar5 files available to access PCI configuration space via regular files.
    git-svn-id: https://freenos.googlecode.com/svn/trunk@314 824000e8-63cf-11de-a6e1-e33d94ba4d19

--- srv/pci/PCIServer.cpp
+++ srv/pci/PCIServer.cpp
@@ -16,14 +16,36 @@
  */
 
 #include <API/ProcessCtl.h>
+#include <Config.h>
+#include <PseudoFile.h>
 #include "PCIServer.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <syslog.h>
-    
-PCIServer::PCIServer()
-    : IPCServer<PCIServer, PCIMessage>(this)
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv)
 {
-    PCIDevice dev;
+    if (!fork())
+    {
+	/* (Re)create target directory. */
+        mkdir("/dev/pci", S_IWUSR | S_IRUSR);
+
+	/*
+	 * Start serving requests.
+	 */
+	PCIServer server("/dev/pci");
+	return server.run();
+    }
+}
+
+PCIServer::PCIServer(const char *path)
+    : FileSystem(path), rootDir(new Directory)
+{
+    /* Set a root directory. */
+    setRoot(rootDir);
 
     /* Open the system log. */
     openlog("PCI", LOG_PID, LOG_USER);
@@ -34,69 +56,148 @@ PCIServer::PCIServer()
 	ProcessCtl(SELF, AllowIO, PCI_CONFADDR + i);
 	ProcessCtl(SELF, AllowIO, PCI_CONFDATA + i);
     }
-    /* Register message handlers. */
-    addIPCHandler(GetDevice, &PCIServer::getDeviceHandler);
-    
     /* Detect PCI host controller. */
     if (PCI_READ_WORD(0, 0, 0, PCI_VID) == 0xffff ||
         PCI_READ_WORD(0, 0, 0, PCI_DID) == 0xffff)
     {
 	syslog(LOG_INFO, "no host controller found");
-	exit(1);
+	exit(EXIT_FAILURE);
     }
     else
     {
 	syslog(LOG_INFO, "host controller found");
+	scan();
     }
-    
+}
+
+void PCIServer::scan()
+{
+    u16 vendorID, deviceID, rev, irq;
+    u32 bar0, bar1, bar2, bar3, bar4, bar5;    
+    Directory *busDir = ZERO, *slotDir = ZERO;
+
     /* Scan the PCI bus. */
     for (u16 bus = 0; bus < 0xff; bus++)
     {
 	for (u16 slot = 0; slot < 0x8; slot++)
         {
 	    /* Read device ID's. */
-    	    dev.vendorID = PCI_READ_WORD(bus, slot, 0, PCI_VID);
-    	    dev.deviceID = PCI_READ_WORD(bus, slot, 0, PCI_DID);
+    	    vendorID = PCI_READ_WORD(bus, slot, 0, PCI_VID);
+    	    deviceID = PCI_READ_WORD(bus, slot, 0, PCI_DID);
 
 	    /* Is this a valid device? */
-    	    if (dev.vendorID != 0xffff &&
-	        dev.deviceID != 0xffff)
+    	    if (vendorID != 0xffff && deviceID != 0xffff)
     	    {
 		/* Read out configuration. */
-		dev.revisionID = PCI_READ_BYTE(bus, slot, 0, PCI_RID);
-		dev.irq        = PCI_READ_BYTE(bus, slot, 0, PCI_IRQ);
-		dev.bar0       = (u8 *) PCI_READ_LONG(bus, slot, 0, PCI_BAR0);
-		dev.bar1       = (u8 *) PCI_READ_LONG(bus, slot, 0, PCI_BAR1);
-		dev.bar2       = (u8 *) PCI_READ_LONG(bus, slot, 0, PCI_BAR2);
-		dev.bar3       = (u8 *) PCI_READ_LONG(bus, slot, 0, PCI_BAR3);
-		dev.bar4       = (u8 *) PCI_READ_LONG(bus, slot, 0, PCI_BAR4);
-		dev.bar5       = (u8 *) PCI_READ_LONG(bus, slot, 0, PCI_BAR5);
-		
-		/* Append to list of devices. */
-		devices.insertTail(new PCIDevice(&dev));
+		rev  = PCI_READ_BYTE(bus, slot, 0, PCI_RID);
+		irq  = PCI_READ_BYTE(bus, slot, 0, PCI_IRQ);
+		bar0 = PCI_READ_LONG(bus, slot, 0, PCI_BAR0);
+		bar1 = PCI_READ_LONG(bus, slot, 0, PCI_BAR1);
+		bar2 = PCI_READ_LONG(bus, slot, 0, PCI_BAR2);
+		bar3 = PCI_READ_LONG(bus, slot, 0, PCI_BAR3);
+		bar4 = PCI_READ_LONG(bus, slot, 0, PCI_BAR4);
+		bar5 = PCI_READ_LONG(bus, slot, 0, PCI_BAR5);
 
-		/* Log the device. */
-	        syslog(LOG_INFO, "[%x:%x] %x:%x (rev %d)",
-        	       bus, slot,    dev.vendorID,
-		       dev.deviceID, dev.revisionID);
+		/* Create bus directory, if needed. */
+		if (!busDir)
+		{
+		    busDir = new Directory;
+		    rootDir->insert(DirectoryFile, "%x", bus);
+		    insertFileCache(busDir, "%x", bus);
+		}
+		/* Create slot directory. */
+		slotDir = new Directory;
+		slotDir->insert(DirectoryFile, ".");
+		slotDir->insert(DirectoryFile, "..");
+		slotDir->insert(RegularFile, "vendor");
+		slotDir->insert(RegularFile, "device");
+		slotDir->insert(RegularFile, "revision");
+		slotDir->insert(RegularFile, "interrupt");
+		slotDir->insert(RegularFile, "bar0");
+		slotDir->insert(RegularFile, "bar1");
+		slotDir->insert(RegularFile, "bar2");
+		slotDir->insert(RegularFile, "bar3");
+		slotDir->insert(RegularFile, "bar4");
+		slotDir->insert(RegularFile, "bar5");
+
+		/* Add the slot directory to the bus directory. */
+		busDir->insert(DirectoryFile, "%x", slot);
+		
+    	        /* Insert into the cache. */
+	        insertFileCache(slotDir, "%x/%x", bus, slot);
+		insertFileCache(new PseudoFile("%x", vendorID),
+				"%x/%x/vendor", bus, slot);
+		insertFileCache(new PseudoFile("%x", deviceID),
+				"%x/%x/device", bus, slot);
+		insertFileCache(new PseudoFile("%u", rev),
+				"%x/%x/revision",  bus, slot);
+		insertFileCache(new PseudoFile("%u", irq),
+				"%x/%x/interrupt", bus, slot);
+		insertFileCache(new PseudoFile("%x", bar0),
+				"%x/%x/bar0", bus, slot);
+		insertFileCache(new PseudoFile("%x", bar1),
+				"%x/%x/bar1", bus, slot);
+		insertFileCache(new PseudoFile("%x", bar2),
+				"%x/%x/bar2", bus, slot);
+		insertFileCache(new PseudoFile("%x", bar3),
+				"%x/%x/bar3", bus, slot);
+		insertFileCache(new PseudoFile("%x", bar4),
+				"%x/%x/bar4", bus, slot);
+		insertFileCache(new PseudoFile("%x", bar5),
+				"%x/%x/bar5", bus, slot);
+
+	        /* Log the device. */
+		syslog(LOG_INFO, "[%x:%x] %x:%x (rev %d)",
+    		       bus, slot, vendorID, deviceID, rev);
     	    }
+	    slotDir = ZERO;
 	}
+	busDir  = ZERO;
     }
 }
 
-void PCIServer::getDeviceHandler(PCIMessage *msg)
+void PCIServer::runDeviceServer(u16 vendorID, u16 deviceID)
 {
-    msg->result = ENOENT;
+    ProcessID pid;
+    struct stat st;
+    char path[PATHLEN];
+    const char *args[4];
+    int status;
 
-    /* Lookup the device. */
-    for (ListIterator<PCIDevice> i(&devices); i.hasNext(); i++)
+    /*
+     * Attempt to execute an external driver program
+     * to handle I/O for the PCI device. First we construct
+     * the full path to the handler, if any.
+     */
+    snprintf(path, sizeof(path), "/srv/pci/%x/%x",
+	     vendorID, deviceID);
+
+    /* If there is no device server, forget it. */
+    if (stat(path, &st) == -1)
     {
-	if (i.current()->vendorID == msg->dev.vendorID &&
-	    i.current()->deviceID == msg->dev.deviceID)
-	{
-	    msg->dev     = *i.current();
-	    msg->result = ESUCCESS;
-	    break;
-	}
+	return;
     }
+
+    /*
+     * Construct an argument list.
+     */
+    for (int i = 0; i < 3; i++)
+    {
+	args[i] = new char[PATHLEN];
+    }
+    args[3] = ZERO;
+    snprintf((char*)args[0], PATHLEN, "%s", path);
+    snprintf((char*)args[1], PATHLEN, "%x", vendorID);
+    snprintf((char*)args[2], PATHLEN, "%x", deviceID);
+
+    /*
+     * Try to fork off a child for the device server.
+     */
+    if ((pid = forkexec(path, args)) == (pid_t) -1)
+    {
+	syslog(LOG_ERR, "failed to forkexec() `%s': %s\r\n",
+			 path, strerror(errno));
+    }
+    else
+	waitpid(pid, &status, 0);
 }
--- srv/pci/PCIServer.h
+++ srv/pci/PCIServer.h
@@ -18,35 +18,171 @@
 #ifndef __PCI_PCISERVER_H
 #define __PCI_PCISERVER_H
 
-#include <IPCServer.h>
+#include <FreeNOS/CPU.h>
+#include <FileSystem.h>
+#include <Directory.h>
 #include <Types.h>
 #include <Error.h>
-#include <List.h>
-#include "PCIDevice.h"
-#include "PCIMessage.h"
+
+/**                                                                                                                                                                                                      
+ * @defgroup pci PCI (Peripheral Component Interconnect) 
+ * @{ 
+ */
 
 /**
- * PCI host controller server.
+ * @name PCI I/O ports.
+ * @{
  */
-class PCIServer : public IPCServer<PCIServer, PCIMessage>
+
+/** @brief Intel's PCI Host controller: address port. */
+#define PCI_CONFADDR	0xcf8
+
+/** @brief Intel's PCI Host controller: data port. */
+#define PCI_CONFDATA	0xcfc
+
+/**
+ * @}
+ */
+
+/**
+ * @name PCI Registers
+ * @{
+ */
+
+/** @brief Vendor Identity. */
+#define PCI_VID	 0x0
+
+/** @brief Device Identity. */
+#define PCI_DID  0x2
+
+/** @brief Command. */
+#define PCI_CMD	 0x4
+
+/** @brief Revision Identity. */
+#define PCI_RID	 0x8
+
+/** @brief Base Address Register 0. */
+#define PCI_BAR0 0x10
+
+/** @brief Base Address Register 1. */
+#define PCI_BAR1 0x14
+
+/** @brief Base Address Register 2. */
+#define PCI_BAR2 0x18
+
+/** @brief Base Address Register 3. */
+#define PCI_BAR3 0x1c
+
+/** @brief Base Address Register 4. */
+#define PCI_BAR4 0x20
+
+/** @brief Base Address Register 5. */
+#define PCI_BAR5 0x24
+
+/** @brief Interrupt Request Vector. */
+#define PCI_IRQ	 0x3c
+
+/**
+ * @}
+ */
+
+/**
+ * @name PCI Macros
+ * @{
+ */
+
+/**
+ * @brief Calculate an address suitable for the PCI_CONFADDR I/O port.
+ */
+#define PCI_ADDRESS(bus, dev, func, reg) \
+    ((((bus)  << 16) & 0xff0000) | \
+     (((dev)  << 11) & 0x00f800) | \
+     (((func) << 8)  & 0x000700) | \
+      ((reg) & 0xfc) | \
+     (0x80000000))
+    
+/**
+ * @brief Read a single byte from PCI configuration space.
+ */
+#define PCI_READ_BYTE(bus, dev, func, reg) \
+    ({ \
+	outl(PCI_CONFADDR, PCI_ADDRESS(bus, dev, func, reg)); \
+	inb(PCI_CONFDATA + ((reg) & 3)); \
+    })
+
+/**
+ * @brief Read a word from PCI configuration space.
+ */
+#define PCI_READ_WORD(bus, dev, func, reg) \
+    (PCI_READ_BYTE(bus, dev, func, reg) |  \
+     PCI_READ_BYTE(bus, dev, func, reg+1) << 8)
+
+/**
+ * @brief Read a long from the PCI configuration space. */
+#define PCI_READ_LONG(bus, dev, func, reg) \
+    (PCI_READ_WORD(bus, dev, func, reg) | \
+     PCI_READ_WORD(bus, dev, func, reg+2) << 16)
+
+/**
+ * @brief Write a single byte to PCI configuration space.
+ */
+#define PCI_WRITE_BYTE(bus, dev, func, reg, val) \
+    ({ \
+	outl(PCI_CONFADDR, PCI_ADDRESS(bus, dev, func, reg)); \
+	outb(PCI_CONFDATA + ((reg) & 3), val); \
+    })
+
+/**
+ * @brief Write a word to PCI configuration space.
+ */
+#define PCI_WRITE_WORD(bus, dev, func, reg, val) \
+    (PCI_WRITE_BYTE(bus, dev, func, reg, val), \
+     PCI_WRITE_BYTE(bus, dev, func, reg+1, val))
+
+/**
+ * @brief Write a long to the PCI configuration space.
+ */
+#define PCI_WRITE_LONG(bus, dev, func, reg, val) \
+    (PCI_WRITE_WORD(bus, dev, func, reg, val), \
+     PCI_WRITE_WORD(bus, dev, func, reg+2, val))
+
+/**
+ * @}
+ */
+
+/**
+ * @brief Peripheral Component Interface FileSystem server.
+ */
+class PCIServer : public FileSystem
 {
     public:
     
+	/** 
+         * Class constructor function. 
+         * @param path Path to which we are mounted. 
+         */
+        PCIServer(const char *path);
+	
 	/**
-	 * Class constructor function.
+	 * @brief (Re)scans the PCI bus.
 	 */
-	PCIServer();
-
+	void scan();
+	
     private:
-
+    
 	/**
-	 * Retrieve PCI device information.
-	 * @param msg Request message.
+	 * @brief Runs a PCI device server.
+	 * @param vendorID Numeric vendor ID read from PCI configuration space.
+	 * @param deviceID Numeric device ID read from PCI configuration space.
 	 */
-	void getDeviceHandler(PCIMessage *msg);
+	void runDeviceServer(u16 vendorID, u16 deviceID);
 
-	/** Contains all detected devices. */
-	List<PCIDevice> devices;
+	/** @brief Root directory pointer. */
+	Directory *rootDir;
 };
+
+/**
+ * @}
+ */
 
 #endif /* __PCI_PCISERVER_H */
