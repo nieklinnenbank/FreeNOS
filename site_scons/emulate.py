63ea87a857a98a9f4efc68b86fa25c5701ea5cf5
nieklinnenbank <nieklinnenbank@824000e8-63cf-11de-a6e1-e33d94ba4d19>
2009-08-16 21:28:43 +0000

    Qemu now starts with -usb per default. This allows us to test the USB UHCI Host Controller and eventually use it as persistent storage device for FreeNOS.
    git-svn-id: https://freenos.googlecode.com/svn/trunk@318 824000e8-63cf-11de-a6e1-e33d94ba4d19

--- site_scons/emulate.py
+++ site_scons/emulate.py
@@ -20,9 +20,9 @@ import build
 
 build.target.Append(ENV = os.environ)
 build.PhonyTargets(build.target,
-		   qemu        = 'qemu -cdrom boot/boot.iso -s',
-		   qemu_debug  = 'qemu -curses -cdrom boot/boot.iso -s -S -d int,pcall,exec',
-		   qemu_curses = 'qemu -curses -cdrom boot/boot.iso -s',
+		   qemu        = 'qemu -usb -cdrom boot/boot.iso -s',
+		   qemu_debug  = 'qemu -curses -usb -cdrom boot/boot.iso -s -S -d int,pcall,exec',
+		   qemu_curses = 'qemu -curses -usb -cdrom boot/boot.iso -s',
 		   qemu_sdl    = 'qemu -sdl -cdrom boot/boot.iso -s',
 		   bochs       = 'bochs -q "ata0-slave: type=cdrom, path=boot/boot.iso, status=inserted" ' \
 				           '"boot: cdrom"',
