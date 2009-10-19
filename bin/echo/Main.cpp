105303a55183c74345684a923e4e6789839df271
nieklinnenbank <nieklinnenbank@824000e8-63cf-11de-a6e1-e33d94ba4d19>
2009-10-19 22:58:23 +0000

    Corrected code indentation to 4 spaces in the echo program.
    git-svn-id: https://freenos.googlecode.com/svn/trunk@365 824000e8-63cf-11de-a6e1-e33d94ba4d19

--- bin/echo/Main.cpp
+++ bin/echo/Main.cpp
@@ -21,20 +21,20 @@
 
 int main(int argc, char **argv)
 {
-	int i, newline = 1, args = 1;
+    int i, newline = 1, args = 1;
 
-	for (i = 1; i < argc; i++)
+    for (i = 1; i < argc; i++)
+    {
+	if (args == 1 && strcmp(argv[i], "-n") == 0)
 	{
-		if (args == 1 && strcmp(argv[i], "-n") == 0)
-		{
-			newline = 0;
-			continue;
-		}
-		args = 0;
-		printf("%s ", argv[i]);
+	    newline = 0;
+	    continue;
 	}
-	if (newline)
-		printf("\n");
+	args = 0;
+	printf("%s ", argv[i]);
+    }
+    if (newline)
+	printf("\n");
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
