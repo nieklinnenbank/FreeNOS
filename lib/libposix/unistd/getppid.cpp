4c5db8e483bfb358bc0351403448d95762f86e14
Niek Linnenbank <nieklinnenbank@gmail.com>
2015-05-15 13:26:05 +0200

    Implemented getppid() for libposix to retrieve the parent process ID.

---
+++ lib/libposix/unistd/getppid.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2009 Niek Linnenbank
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <FreeNOS/API.h>
+#include <CoreMessage.h>
+#include <UserProcess.h>
+#include "unistd.h"
+
+pid_t getppid()
+{
+    CoreMessage msg;
+    UserProcess info;
+
+    msg.action = ReadProcess;
+    msg.buffer = &info;
+    msg.number = getpid();
+    msg.ipc(CORESRV_PID, API::SendReceive, sizeof(msg));
+
+    return info.parent;
+}
