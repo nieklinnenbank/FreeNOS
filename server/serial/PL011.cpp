9538bd61ef8a82f704512522f2f7cc7a04554ed9
Niek Linnenbank <nieklinnenbank@gmail.com>
2015-07-09 19:57:48 +0200

    Implemented PL011 serial UART for ARM.

---
+++ server/serial/PL011.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright (C) 2015 Niek Linnenbank
+ * Copyright (C) 2013 Goswin von Brederlow <goswin-v-b@web.de>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <FreeNOS/System.h>
+#include "PL011.h"
+
+#warning place memory barriers please!!!
+
+PL011::PL011(u32 irq)
+{
+    m_irq = irq;
+}
+
+Error PL011::initialize()
+{
+    // TODO: hack. disable IRQ_REG() == 0...
+    ProcessCtl(SELF, DisableIRQ, 0);
+
+    // Disable PL011.
+    IO::write(PL011_CR, 0x00000000);
+    
+    // Setup the GPIO pin 14 && 15.
+    // Disable pull up/down for all GPIO pins & delay for 150 cycles.
+    IO::write(GPPUD, 0x00000000);
+    delay(150);
+
+    // Disable pull up/down for pin 14,15 & delay for 150 cycles.
+    IO::write(GPPUDCLK0, (1 << 14) | (1 << 15));
+    delay(150);
+
+    // Write 0 to GPPUDCLK0 to make it take effect.
+    IO::write(GPPUDCLK0, 0x00000000);
+    
+    // Clear pending interrupts.
+    IO::write(PL011_ICR, 0x7FF);
+
+    // Set integer & fractional part of baud rate.
+    // Divider = UART_CLOCK/(16 * Baud)
+    // Fraction part register = (Fractional part * 64) + 0.5
+    // UART_CLOCK = 3000000; Baud = 115200.
+
+    // Divider = 3000000/(16 * 115200) = 1.627 = ~1.
+    // Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
+    IO::write(PL011_IBRD, 1);
+    IO::write(PL011_FBRD, 40);
+
+    // Disable FIFO, use 8 bit data transmission, 1 stop bit, no parity
+    IO::write(PL011_LCRH, PL011_LCRH_WLEN_8BIT);
+
+    // Enable Rx/Tx interrupts
+    IO::write(PL011_IMSC,
+         PL011_IMSC_RXIM); // | PL011_IMSC_TXIM);
+
+    // Enable PL011, receive & transfer part of UART.
+    IO::write(PL011_CR, (1 << 0) | (1 << 8) | (1 << 9));
+    return 0;
+}
+
+Error PL011::interrupt(u32 vector)
+{
+    // Clear Receive Interrupts
+    u32 mis = IO::read(PL011_MIS);
+    if (mis & PL011_MIS_RXMIS)
+        IO::write(PL011_ICR, PL011_ICR_RXIC);
+
+    // Clear Transmit Interrupts
+    mis = IO::read(PL011_MIS);
+    if (mis & PL011_MIS_TXMIS)
+        IO::write(PL011_ICR, PL011_ICR_TXIC);
+
+    // Re-enable interrupts
+    ProcessCtl(SELF, EnableIRQ, m_irq);
+    return ESUCCESS;
+}
+
+Error PL011::read(s8 *buffer, Size size, Size offset)
+{
+    Size bytes = 0;
+
+    // Clear Receive Interrupts
+    u32 mis = IO::read(PL011_MIS);
+    if (mis & PL011_MIS_RXMIS)
+        IO::write(PL011_ICR, PL011_ICR_RXIC);
+
+    // Read as much bytes as possible
+    while (!(IO::read(PL011_FR) & PL011_FR_RXFE) && bytes < size)
+    {
+        buffer[bytes++] = IO::read(PL011_DR);
+    }
+    return bytes ? (Error) bytes : EAGAIN;
+}
+
+Error PL011::write(s8 *buffer, Size size, Size offset)
+{
+    Size bytes = 0;
+
+    // Clear Transmit Interrupts
+    u32 mis = IO::read(PL011_MIS);
+    if (mis & PL011_MIS_TXMIS)
+        IO::write(PL011_ICR, PL011_ICR_TXIC);
+
+    // Write as much bytes as possible
+    while ((IO::read(PL011_FR) & PL011_FR_TXFE) && bytes < size)
+    {
+        IO::write(PL011_DR, buffer[bytes++]);
+    }
+    return bytes ? (Error) bytes : EAGAIN;
+}
+
+void PL011::delay(s32 count)
+{
+    asm volatile("1: subs %[count], %[count], #1; bne 1b"
+         : : [count]"r"(count));
+}
---
+++ server/serial/PL011.h
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2015 Niek Linnenbank
+ * Copyright (C) 2013 Goswin von Brederlow <goswin-v-b@web.de>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __ARM_RASPISERIAL_H
+#define __ARM_RASPISERIAL_H
+
+#include <FreeNOS/API.h>
+#include <FreeNOS/System.h>
+#include <Log.h>
+#include <Types.h>
+#include <Device.h>
+
+class PL011 : public Device
+{
+  public:
+
+    /**
+     * Constructor.
+     */
+    PL011(u32 irq);
+
+    /**
+     * @brief Initializes the i8250 serial UART.
+     * @return Error status code.
+     */
+    virtual Error initialize();
+
+    /**
+     * Called when an interrupt has been triggered for this device.
+     * @param vector Vector number of the interrupt.
+     * @return Error result code.
+     */
+    virtual Error interrupt(Size vector);
+
+    /** 
+     * Read bytes from the i8250.
+     * @param buffer Buffer to save the read bytes.
+     * @param size Number of bytes to read.
+     * @param offset Unused.
+     * @return Number of bytes on success and ZERO on failure. 
+     */
+    virtual Error read(s8 *buffer, Size size, Size offset);
+
+    /** 
+     * Write bytes to the device.
+     * @param buffer Buffer containing bytes to write. 
+     * @param size Number of bytes to write.
+     * @param offset Unused.
+     * @return Number of bytes on success and ZERO on failure. 
+     */
+    virtual Error write(s8 *buffer, Size size, Size offset);
+
+  private:
+
+    enum {
+        // The GPIO registers base address.
+        GPIO_BASE = 0x20200000,
+
+        // The offsets for reach register.
+
+        // Controls actuation of pull up/down to ALL GPIO pins.
+        GPPUD = (GPIO_BASE + 0x94),
+
+        // Controls actuation of pull up/down for specific GPIO pin.
+        GPPUDCLK0 = (GPIO_BASE + 0x98),
+
+        // The base address for UART.
+        PL011_BASE = 0x20201000,
+
+        // The offsets for reach register for the UART.
+        PL011_DR     = (PL011_BASE + 0x00),
+        PL011_RSRECR = (PL011_BASE + 0x04),
+        PL011_FR     = (PL011_BASE + 0x18),
+        PL011_FR_RXFE = (1 << 4),
+        PL011_FR_TXFE = (1 << 7),
+
+        PL011_ILPR   = (PL011_BASE + 0x20),
+        PL011_IBRD   = (PL011_BASE + 0x24),
+        PL011_FBRD   = (PL011_BASE + 0x28),
+        PL011_LCRH   = (PL011_BASE + 0x2C),
+        PL011_LCRH_WLEN_8BIT = (0b11<<5),
+
+        PL011_CR     = (PL011_BASE + 0x30),
+        PL011_IFLS   = (PL011_BASE + 0x34),
+        PL011_IMSC   = (PL011_BASE + 0x38),
+        PL011_IMSC_RXIM = (1 << 4),
+        PL011_IMSC_TXIM = (1 << 5),
+
+        PL011_RIS    = (PL011_BASE + 0x3C),
+        PL011_MIS    = (PL011_BASE + 0x40),
+
+        PL011_MIS_RXMIS = (1 << 4),
+        PL011_MIS_TXMIS = (1 << 5),
+
+        PL011_ICR    = (PL011_BASE + 0x44),
+        PL011_ICR_TXIC = (1 << 5),
+        PL011_ICR_RXIC = (1 << 4),
+
+        PL011_DMACR  = (PL011_BASE + 0x48),
+        PL011_ITCR   = (PL011_BASE + 0x80),
+        PL011_ITIP   = (PL011_BASE + 0x84),
+        PL011_ITOP   = (PL011_BASE + 0x88),
+        PL011_TDR    = (PL011_BASE + 0x8C),
+    };
+
+    /*
+     * delay function
+     * int32_t delay: number of cycles to delay
+     *
+     * This just loops <delay> times in a way that the compiler
+     * wont optimize away.
+     */
+    static void delay(s32 count);
+
+    /** interrupt vector */
+    u32 m_irq;
+};
+
+#endif /* __ARM_PL011_H */
