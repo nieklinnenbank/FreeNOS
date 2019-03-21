c588ab03dbf8f97ba0caba48499e7d7cb3c47ef5
Niek Linnenbank <nieklinnenbank@gmail.com>
2019-03-21 21:37:45 +0100

    kernel: process: remove TODO about m_parent

--- kernel/Process.cpp
+++ kernel/Process.cpp
@@ -31,7 +31,7 @@ Process::Process(ProcessID id, Address entry, bool privileged, const MemoryMap &
     m_kernelStack   = 0;
     m_userStack     = 0;
     m_pageDirectory = 0;
-    m_parent        = 0; // TODO: wrong? should be -1 or NONE instead?
+    m_parent        = 0;
     m_waitId        = 0;
     m_wakeups       = 0;
     m_entry         = entry;
