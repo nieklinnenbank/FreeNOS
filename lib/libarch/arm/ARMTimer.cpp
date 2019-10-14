08680d69f983163786d7cfbe08681e71bcd554ae
Niek Linnenbank <nieklinnenbank@gmail.com>
2019-10-14 22:35:27 +0200

    libarch: arm: set m_int value to ARMTIMER_IRQ

--- lib/libarch/arm/ARMTimer.cpp
+++ lib/libarch/arm/ARMTimer.cpp
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <FreeNOS/System.h>
 #include <Log.h>
 #include "ARMCore.h"
 #include "ARMTimer.h"
@@ -25,6 +26,7 @@
 ARMTimer::ARMTimer()
     : m_frequency(0)
 {
+    m_int = ARMTIMER_IRQ;
 }
 
 u32 ARMTimer::getSystemFrequency(void) const
