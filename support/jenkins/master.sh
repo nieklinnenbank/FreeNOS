b3ca66e35c6dd28532c8b00c9d46ab6bd70c9444
Niek Linnenbank <nieklinnenbank@gmail.com>
2019-10-27 22:17:27 +0100

    support: jenkins: rename all ubuntu-1804 references to ubuntu1804

--- support/jenkins/master.sh
+++ support/jenkins/master.sh
@@ -99,14 +99,14 @@ mkdir -p $JENKINS_HOME/jobs
 mkdir -p $JENKINS_HOME/jobs/FreeNOS-ubuntu1804
 mkdir -p $JENKINS_HOME/jobs/FreeNOS-freebsd12
 mkdir -p $JENKINS_HOME/nodes
-mkdir -p $JENKINS_HOME/nodes/ubuntu-1804
+mkdir -p $JENKINS_HOME/nodes/ubuntu1804
 mkdir -p $JENKINS_HOME/nodes/freebsd12
 mkdir -p $JENKINS_HOME/secrets
 mv ~vagrant/master.key $JENKINS_HOME/secrets/
 mv ~vagrant/hudson.util.Secret $JENKINS_HOME/secrets/
 mv ~vagrant/credentials.xml $JENKINS_HOME/
-mv ~vagrant/ubuntu-1804.job.xml $JENKINS_HOME/jobs/FreeNOS-ubuntu1804/config.xml
-mv ~vagrant/ubuntu-1804.node.xml $JENKINS_HOME/nodes/ubuntu-1804/config.xml
+mv ~vagrant/ubuntu1804.job.xml $JENKINS_HOME/jobs/FreeNOS-ubuntu1804/config.xml
+mv ~vagrant/ubuntu1804.node.xml $JENKINS_HOME/nodes/ubuntu1804/config.xml
 mv ~vagrant/freebsd12.job.xml $JENKINS_HOME/jobs/FreeNOS-freebsd12/config.xml
 mv ~vagrant/freebsd12.node.xml $JENKINS_HOME/nodes/freebsd12/config.xml
 
