9ec1f7408e146b837db86acd58b58ed89c05fd0d
Niek Linnenbank <nieklinnenbank@gmail.com>
2020-05-04 22:15:50 +0200

    config/arm/raspbery(2): remove outdated NFC, I2C and SPI implementations

--- config/arm/raspberry2/init.sh
+++ config/arm/raspberry2/init.sh
@@ -8,9 +8,6 @@ stdio /dev/serial/serial0/io /dev/serial/serial0/io
 #
 # System Servers and Drivers.
 #
-#/server/i2c/grovepi/server
-#/server/usb/controller/server &
-#/server/usb/hub/server &
 /server/filesystem/tmp/server /tmp &
 /server/network/loopback/server &
 
