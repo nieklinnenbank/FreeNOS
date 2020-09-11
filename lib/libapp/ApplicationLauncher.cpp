a42b2acc982f4138c68c1c5a8c2ffa15b65000f3
Niek Linnenbank <nieklinnenbank@gmail.com>
2020-09-11 22:57:22 +0200

    lib/libapp: add ApplicationLauncher for running external programs

---
+++ lib/libapp/ApplicationLauncher.cpp
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2020 Niek Linnenbank
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Log.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include "ApplicationLauncher.h"
+
+ApplicationLauncher::ApplicationLauncher(const char *path,
+                                         const char **argv)
+    : m_path(path)
+    , m_argv(argv)
+    , m_pid(0)
+{
+}
+
+const ProcessID ApplicationLauncher::getPid() const
+{
+    return m_pid;
+}
+
+ApplicationLauncher::Result ApplicationLauncher::exec()
+{
+    int pid;
+
+#ifdef __HOST__
+    struct stat buf;
+
+    if (stat(*m_path, &buf) != 0)
+    {
+        DEBUG("stat failed for " << *m_path << ": " << strerror(errno));
+        return NotFound;
+    }
+
+    pid = fork();
+    switch (pid)
+    {
+        case 0:
+            execve(*m_path, (char * const *)m_argv, (char * const *)NULL);
+            ERROR("execve failed for " << *m_path << ": " << strerror(errno));
+            exit(1);
+            break;
+
+        case -1:
+            ERROR("fork failed for " << *m_path << ": " << strerror(errno));
+            break;
+
+        default:
+            break;
+    }
+#else
+    pid = forkexec(*m_path, m_argv);
+    if (pid == -1)
+    {
+        DEBUG("forkexec failed for " << *m_path << ": " << strerror(errno));
+        return IOError;
+    }
+#endif
+
+    m_pid = pid;
+    return Success;
+}
+
+ApplicationLauncher::Result ApplicationLauncher::terminate() const
+{
+    if (m_pid == 0)
+    {
+        return InvalidArgument;
+    }
+
+    if (::kill(m_pid, SIGTERM) == 0)
+    {
+        return Success;
+    }
+    else
+    {
+        return IOError;
+    }
+}
---
+++ lib/libapp/ApplicationLauncher.h
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2020 Niek Linnenbank
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIB_LIBAPP_APPLICATIONLAUNCHER_H
+#define __LIB_LIBAPP_APPLICATIONLAUNCHER_H
+
+#include <Types.h>
+#include <String.h>
+
+/**
+ * @addtogroup lib
+ * @{
+ *
+ * @addtogroup libapp
+ * @{
+ */
+
+/**
+ * Helper class to launch an external program.
+ */
+class ApplicationLauncher
+{
+  public:
+
+    /**
+     * Result code
+     */
+    enum Result
+    {
+        Success,
+        InvalidArgument,
+        NotFound,
+        IOError
+    };
+
+  public:
+
+    /**
+     * Constructor
+     *
+     * @param path Full path to the program to run
+     * @param argv Array with arguments
+     */
+    ApplicationLauncher(const char *path, const char **argv);
+
+    /**
+     * Retrieve Process Identifier of the program
+     *
+     * @return ProcessID
+     */
+    const ProcessID getPid() const;
+
+    /**
+     * Runs the external program
+     *
+     * @return Result code
+     */
+    Result exec();
+
+    /**
+     * Terminate the program.
+     *
+     * @return Result code
+     */
+    Result terminate() const;
+
+  private:
+
+    /** Absolute path to the program to run */
+    const String m_path;
+
+    /** Array with pointers to program arguments */
+    const char **m_argv;
+
+    /** PID of the DatastoreServer. */
+    ProcessID m_pid;
+};
+
+/**
+ * @}
+ * @}
+ */
+
+#endif /* __LIB_LIBAPP_APPLICATIONLAUNCHER_H */
